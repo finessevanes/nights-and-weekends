fbea5ecd92f7b2fbc4dc9be287c13ac6f0bfe4ce
Vanessa <vanessa.mercado24@gmail.com>
2022-07-02 15:48:15 -0700

    clean up component

--- src/components/BuyTicket.js
+++ src/components/BuyTicket.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { QRCodeSVG } from 'qrcode.react'
+import ticket from './Ticket.svg'
 
-// image on the nft
+
 // button to mint
 
 // ** nice to haves **
@@ -9,32 +10,6 @@ import { QRCodeSVG } from 'qrcode.react'
 // place to see balance in 
 // link that takes them to faucet
 
-const TicketContainerStyle = `
-rounded-lg
-text-white
-h-10
-w-96
-bg-gradient-300
-flex
-flex-col
-`
-const MainStubStyle = `
-border-b-2
-flex
-flex-col
-h-4/5
-text-center
-`
-const SecondaryStubStyle = `
-h-1/5
-w-full
-flex
-justify-end
-items-end
-pr-2
-pb-2
-
-`
 
 const ButtonStyle = `
 bg-gradient-200 hover:bg-gradient-100 text-white py-2 px-4 rounded shadow mt-9 w-1/3 self-center
@@ -45,17 +20,7 @@ const BuyTicket = () => {
   return (
     <>
       <div className='flex self-start flex-col'>
-        <div className={TicketContainerStyle}>
-          <div className={MainStubStyle}>
-            <span className='text-6xl self-start pt-4 pl-4'>🌙 </span>
-            <h1 className='text-yellow-400 text-xTitle mt-16'>NIGHTS & WEEKENDS</h1>
-            <span className='text-x69 rotate-[57deg] animate-pulse mt-16'>🦄</span>
-            <h2 className='flex justify-center h-full items-end text-3xl font-bold mb-2'>SEASON 01</h2>
-          </div>
-          <div className={SecondaryStubStyle}>
-            <QRCodeSVG bgColor='black' fgColor='#060A1F' size='100' value='https://buildspace.so/nights-weekends' />
-          </div>
-        </div>
+        <img src={ticket} alt="SVG logo image"/>
         <button className={ButtonStyle}>Mint Ticket</button>
       </div>
     </>
