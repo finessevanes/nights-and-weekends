ee96a97ad48578d57568e756662ff41fe16ec91e
Vanessa <vanessa.mercado24@gmail.com>
2022-07-03 17:29:42 -0700

    finally - wallectconnect imports w/o errors

--- src/Modal.js
+++ src/Modal.js
@@ -1,16 +1,4 @@
-import {
-    VStack,
-    HStack,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    Text
-} from "@chakra-ui/react";
-import { Image } from "@chakra-ui/react";
+
 import { useWeb3React } from "@web3-react/core";
 import { connectors } from "./connectors";
 
@@ -18,7 +6,7 @@ const ButtonStyle = `
 bg-gradient-200 hover:bg-gradient-250 text-white py-2 px-4 rounded shadow
 `
 
-export default function SelectWalletModal({ isOpen, closeModal }) {
+export default function SelectWalletModal() {
     const { activate } = useWeb3React();
 
     const setProvider = (type) => {
@@ -30,14 +18,12 @@ export default function SelectWalletModal({ isOpen, closeModal }) {
             <button className={ButtonStyle} onClick={() => {
                 activate(connectors.walletConnect);
                 setProvider("walletConnect");
-                closeModal();
             }}>
                 WalletConnect
             </button>
             <button className={ButtonStyle} onClick={() => {
                 activate(connectors.injected);
                 setProvider("injected");
-                closeModal();
             }}>
                 Metamask
             </button>
--- src/connectors.js
+++ src/connectors.js
@@ -1,4 +1,5 @@
-import { InjectedConnector, WalletConnectConnector, WalletLinkConnector } from "@web3-react/injected-connector"
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
@@ -10,13 +11,7 @@ const walletconnect = new WalletConnectConnector({
   qrcode: true
 });
 
-const walletlink = new WalletLinkConnector({
-  url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-  appName: "web3-react-demo"
-});
-
 export const connectors = {
   injected: injected,
-  walletConnect: walletconnect,
-  coinbaseWallet: walletlink
+  walletConnect: walletconnect
 };
